8859064bd9250744c00396c06cb866adf1441c43
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2023-07-01 07:46:03 +0700

    fixing component for display navbar

--- frontend/src/component/Navbar.jsx
+++ frontend/src/component/Navbar.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div className="sticky top-0 z-10">
-        <div className="navbar bg-primary px-5 pt-2">
+        <div className="navbar bg-base-300 border-b-2 border-base-100 px-5 pt-2">
           <div className="navbar-start">
-            <a className="btn btn-ghost normal-case text-xl text-white font-bold">
-              Sistem Informasi Geografis Pemetaan Pondok Pesantren Bantul
-            </a>
+          <a href='http://localhost:3000/' className="btn btn-base-100 bg-base-300 normal-case lg:text-xl md:text-sm text-white font-bold truncate">
+            SIG Pemetaan Pondok Pesantren Bantul
+          </a>
           </div>
           <div className="navbar-end">
-            <a className="btn btn-success font-bold bg-blue-700 border-0 hover:bg-sky-700">Login</a>
+            <a className="btn font-bold bg-blue-500 border-0 text-white hover:bg-base-100">Login</a>
           </div>
         </div>
       </div>
